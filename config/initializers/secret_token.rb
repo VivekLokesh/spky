01577de64e1aa83a42700fa05cc3b6255f3b92ee
Vivek V <vivek.lokesh@gmail.com>
2013-09-14 20:40:15 -0700

    First Ever Git Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spky::Application.config.secret_token = 'c834f11b4cec73470018f158a1bce948571250bb354a2a347920b77dc809f5cd801ec7359b17cf3e64567450f77e93b1395e39fad57920b0f66f2886b6db46ae'
