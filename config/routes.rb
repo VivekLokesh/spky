1cecf92ec836c82155f83ecea0e1a5aed3eeeb19
Vivek V <vivek.lokesh@gmail.com>
2013-09-16 22:19:18 -0700

    Adding Content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Spky::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
+  #  get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
